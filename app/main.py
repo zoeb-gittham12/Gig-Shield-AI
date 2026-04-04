105053469b09709a475988e2d7867eb666e50f3e
Zoeb <zoeb.gittham12@gmail.com>
2026-04-04 18:36:35 +0530

    liawgkjasd

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["https://gigshield-ai-demo.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
